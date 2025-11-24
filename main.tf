f9f1726b90bd69c92155b068a5907781cdb8e8be
christinaotoboh <otobohsmiles@gmail.com>
2025-11-24 20:30:50 +0100

    updates on main.tf

--- main.tf
+++ main.tf
@@ -59,6 +59,38 @@ resource "aws_subnet" "private-2" {
 
 
 
+resource "aws_internet_gateway" "techcorp-igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "main_igw"
+    description = "Internet gateway for the VPC"
+  }
+}
+
+
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.techcorp-igw.id
+  }
+
+  tags = {
+    Name = "rtb_1"
+  }
+}
+
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.rt.id
+}
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.public-2.id
+  route_table_id = aws_route_table.rt.id
+}
+
 
 
 resource "aws_instance" "my_instance" {
